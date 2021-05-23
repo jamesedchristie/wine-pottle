3e1583070da1beeba2527477cbd19ddbb50ac721
jamesedchristie <jamesedchristie1@gmail.com>
2021-05-23 17:53:12 +1000

    Netlify bug

--- .svelte-kit/output/server/app.js
+++ .svelte-kit/output/server/app.js
@@ -78,7 +78,7 @@ const serviceAccount = {
   type: "service_account",
   project_id: "winepottle",
   private_key_id: process.env["FIREBASE_API_KEY"],
-  private_key: process.env["FIREBASE_ADMIN_PRIVATE_KEY"].replace(/\\n/g, "\n"),
+  private_key: process.env["FIREBASE_ADMIN_PRIVATE_KEY"]?.replace(/\\n/g, "\n"),
   client_email: process.env["FIREBASE_ADMIN_CLIENT_EMAIL"],
   client_id: process.env["FIREBASE_ADMIN_CLIENT_ID"],
   auth_uri: "https://accounts.google.com/o/oauth2/auth",
